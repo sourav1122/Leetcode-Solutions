40d3b0eb2e63fff40aa425f8716367b55cda396c
sourav kumar <43821659+sourav1122@users.noreply.github.com>
2021-08-11 20:17:30 +0530

    Time: 720 ms (49.76%), Space: 16.7 MB (30.33%) - LeetHub

--- array-of-doubled-pairs/array-of-doubled-pairs.py
+++ array-of-doubled-pairs/array-of-doubled-pairs.py
@@ -1,41 +1,11 @@
-class Solution:
-    def canReorderDoubled(self, arr: List[int]) -> bool:
-        d={}
-        arr.sort()
-        for i in arr:
-            if i not in d:
-                d[i]=1
-            else:
-                d[i]+=1
-        if 0 in d:
-            if d[0]%2!=0:
-                return 0
-            d[0]=0
-        for i in range(len(arr)) :
-            #print(arr[i])
-            if arr[i]==0:
-                continue
-            #print(  arr[i],"YAHA")
-            if 2*arr[i] not in d and  arr[i]//2 not in d:
-                #print("YAHA")
-                return 0
-            if 2*arr[i] in d and d[arr[i]]!=0 and d[2*arr[i]]!=0:
-                x=min(d[2*arr[i]],d[arr[i]])
-                d[2*arr[i]]-=x
-                d[arr[i]]-=x
-            elif arr[i]//2 in d and d[arr[i]]!=0 and d[arr[i]//2]!=0:
-                x=min(d[arr[i]//2],d[arr[i]])
-                d[arr[i]//2]-=x
-                d[arr[i]]-=x
-            #print(d)
-        #print(d)
-        for i in d:
-            if d[i]!=0:
-                return 0
-        return 1
-                
-                
-                
-            
-            
-        
+class Solution(object):
+    def canReorderDoubled(self, A):
+        count = collections.Counter(A)
+        print(count)
+        for x in sorted(A, key = abs):
+            if count[x] == 0: continue
+            if count[2*x] == 0: return False
+            count[x] -= 1
+            count[2*x] -= 1
+
+        return True
