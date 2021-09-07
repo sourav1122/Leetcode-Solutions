a0359636a7be2a94cbbd0a506085f64500e6a180
sourav kumar <43821659+sourav1122@users.noreply.github.com>
2021-09-08 00:13:25 +0530

    Time: 812 ms (69.50%), Space: 54.6 MB (12.92%) - LeetHub

---
+++ partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        r=sum(nums)//2
+        if sum(nums)%2!=0:
+            return 0
+        d=[[-1 for i in range(r+1)]for j in range(len(nums)+1)]
+        def k(nums,r,pos):
+            if r==0:
+                #print("HA")
+                d[pos][r]=1
+                return d[pos][r]
+            if pos<0:
+                return 0
+            if d[pos][r]!=-1:
+                return d[pos][r]
+            if nums[pos-1]<=r:
+                #print(k(nums,r-nums[pos-1],pos-1),k(nums,r,pos-1))
+                d[pos][r]= k(nums,r-nums[pos-1],pos-1) or k(nums,r,pos-1)
+                return d[pos][r]
+            else:
+                d[pos][r]= k(nums,r,pos-1)
+                return d[pos][r]
+        return k(nums,r,len(nums)-1)
+                
